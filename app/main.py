4f1e8367e827525659abdcbba7823ee2cc9e0576
Milena Linda <milenafernandes594@gmail.com>
2025-04-15 11:46:05 -0300

    fix: add informações nutricionais do alimento selecionado

--- app/main.py
+++ app/main.py
@@ -1,17 +1,15 @@
-# main.py
 from fastapi import FastAPI, Query
 from fastapi.middleware.cors import CORSMiddleware
 import pandas as pd
 from collections import deque
-from fastapi.responses import FileResponse
+from fastapi.responses import FileResponse, JSONResponse
 from fastapi.staticfiles import StaticFiles
 import os
-from fastapi.responses import JSONResponse
+
 
 
 app = FastAPI()
 
-# Permitir chamadas do frontend
 app.add_middleware(
     CORSMiddleware,
     allow_origins=["*"],
@@ -19,18 +17,18 @@ app.add_middleware(
     allow_headers=["*"],
 )
 
-# Carregar o dataset
+
 df = pd.read_parquet("taco-db-nutrientes.parquet")
 nutrientes = ['Proteína (g)', 'Lipídeos (g)', 'Carboidrato (g)', 'Fibra Alimentar (g)', 'Cálcio (mg)']
 
 for col in nutrientes:
     df[col] = pd.to_numeric(df[col], errors='coerce')
 
-# Função de distância
+
 def calcular_similaridade(a1, a2):
     return sum(abs(a1[n] - a2[n]) for n in nutrientes)
 
-# Construir grafo
+
 grafo = {}
 for i, a1 in df.iterrows():
     nome1 = a1['Nome']
@@ -50,7 +48,7 @@ def substitutos_bfs(inicio, max_resultados=5):
     fila = deque([inicio])
     substitutos = []
 
-    # Buscar o alimento inicial no dataset
+  
     alimento_info = df[df['Nome'] == inicio][['Nome'] + nutrientes]
     if alimento_info.empty:
         return {"erro": "Alimento inicial não encontrado no dataset"}
@@ -67,7 +65,7 @@ def substitutos_bfs(inicio, max_resultados=5):
                 if vizinho not in visitados:
                     fila.append(vizinho)
 
-                    # Obter as informações nutricionais do vizinho
+                   
                     substituto_info = df[df['Nome'] == vizinho][['Nome'] + nutrientes]
                     if not substituto_info.empty:
                         substituto_info = substituto_info.to_dict(orient="records")[0]
@@ -98,7 +96,7 @@ def obter_substitutos(alimento: str):
     return resultado
 
 
-# Servir index.html
+
 app.mount("/static", StaticFiles(directory="static"), name="static")
 
 @app.get("/")
